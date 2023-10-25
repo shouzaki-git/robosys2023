22707a46a990788abbc1a7a42674dd440c7938c4
Sho Uzaki <sho.uzaki@icloud.com>
2023-10-26 00:49:42 +0900

    add a STRANGE INPUT

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
-#1/bin/bush
+#1/bin/bush -xv
+# SPDX-FileCopyrightText: 2023 Sho Uzaki
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
         echo NG at LINE "$1"
@@ -7,9 +9,18 @@ ng () {
 
 ret=0
 
+### I/O ###
 out=$(seq 5 | ./plus)
-
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng {LINENO}
+[ "${out}" = "" ] || ng {LINENO}    
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$ret" = 0 ] && echo OK
 exit $ret
