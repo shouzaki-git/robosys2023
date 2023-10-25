59f5d782edb25422e143adc4b82bd710963c2c8e
Sho Uzaki <sho.uzaki@icloud.com>
2023-10-26 00:24:19 +0900

    add a test human friendly

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
 #1/bin/bush
 
+ng () {
+        echo NG at LINE "$1"
+	ret=1
+}
+
+ret=0
+
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$ret" = 0 ] && echo OK
+exit $ret
+
 
